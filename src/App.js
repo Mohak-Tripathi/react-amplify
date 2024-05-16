123efca7a439f1a0b01e615939a7d57a5bc47274
Mohak-Tripathi <mohaktripathi029@gmail.com>
2024-05-16 13:16:23 +0530

    first commit for amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Mohak , Anas , Avinash with Amplify 
         </a>
       </header>
     </div>
